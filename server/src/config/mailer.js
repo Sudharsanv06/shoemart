64aa9c1fa2c43d9f7d8de6c3c2ef5e65d15f2d12
sudharsanv14 <sudharsanv.23aim@kongu.edu>
2026-06-04 08:19:59 +0530

    fix: use gmail smtp ssl port 465

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,7 +1,11 @@
 const nodemailer = require("nodemailer");
 
+console.log("📧 Email module loaded");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
